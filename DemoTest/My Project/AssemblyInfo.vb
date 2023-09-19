6b7c9005faa4a9beb8bc3f12881879a2ec781726
Dwaraka Chitty <Dwaraka.Chitty@acgov.org>
2023-09-19 10:22:38 -0700

    Add project files.

---
+++ DemoTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("DemoTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Alameda County")>
+<Assembly: AssemblyProduct("DemoTest")>
+<Assembly: AssemblyCopyright("Copyright © Alameda County 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c18caf82-db5c-4cc9-8c39-c7e111fc1997")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
